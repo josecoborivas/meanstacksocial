2d94f750e45f481304f16150908344d72d7e1c25
Jose Cobo <josecoborivas@gmail.com>
2018-03-16 20:12:21 -0300

    conexion a la base de datos mongodb

---
+++ api/index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost:27017/curso_mean_social', {useMongoClient: true})
+        .then(()=>{
+            console.log('La conexion a la base de datos curso_mean_social se ha realizado correctamente!')
+        })
+        .catch(err=>console.log(err));
+
