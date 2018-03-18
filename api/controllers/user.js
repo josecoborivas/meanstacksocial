85c78253151436b40aae3385b0e279886ee66684
Jose Cobo <josecoborivas@gmail.com>
2018-03-18 13:09:20 -0300

    Metodo register en el controlador user

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var bcrypt = require('bcrypt-nodejs');
+
 var User = require('../models/user');
 
 
@@ -16,7 +18,42 @@ function pruebas(req, res){
     });
 }
 
+function saveUser(req, res){
+    var params = req.body;
+    var user = new User();
+
+    if(params.name && params.surname && params.nick && params.email && params.password){
+        user.name = params.name;
+        user.surname = params.surname;
+        user.nick = params.nick;
+        user.email = params.email;
+        user.role = 'ROLE_USER';
+        user.image = null;
+
+        bcrypt.hash(params.password, null, null, (err, hash) =>{
+            user.password = hash;
+
+            user.save((err, userStored) =>{
+                if(err) return res.status(500).send({message: 'Error al guardar el usuario'});
+
+                if(userStored){
+                    res.status(200).send({user: userStored});
+                }else{
+                    res.status(404).send({message: 'No se ha registrado el usuario'});
+                }
+            });
+        });
+    }else{
+        res.status(400).send({message:'Envia todos los campos necesarios'});
+    }
+
+    
+
+
+}
+
 module.exports = {
     home,
-    pruebas
+    pruebas,
+    saveUser
 }
