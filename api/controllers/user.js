1764922de210a7e43f020b5dd895a870b370b53c
Jose Cobo <josecoborivas@gmail.com>
2018-03-19 13:48:24 -0300

    Metodo para actualizar los datos de un usuario

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -139,11 +139,33 @@ function getUsers(req, res){
     });
 }
 
+//Edicion de un usuario
+function updateUser(req, res){
+    var userId = req.params.id;
+    var update = req.body;
+
+    //Borrar la propiedad password
+    delete update.password;
+
+    if(userId != req.user.sub){
+        return res.status(500).send({message: 'No tienes permiso para actualizar los datos del usuario'});
+    }
+
+    User.findByIdAndUpdate(userId, update, {new:true}, (err, userUpdate) =>{
+        if(err) return res.status(500).send({message: 'Error en la peticion'});
+
+        if(!userUpdate) return res.status(404).send({message: 'No se ha podido actualizar el usuario'});
+
+        return res.status(200).send({user: userUpdate});
+    });
+}
+
 module.exports = {
     home,
     pruebas,
     saveUser,
     loginUser,
     getUser,
-    getUsers
+    getUsers,
+    updateUser
 }
