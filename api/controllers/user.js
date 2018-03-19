9ffab76deb7bffebc29a80f58055197e946fd8fd
Jose Cobo <josecoborivas@gmail.com>
2018-03-19 11:34:35 -0300

    Servicio jwt para generar token de usuario. Usar este servicio para generar y devolver el token en el loginUser.

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -4,6 +4,8 @@ var bcrypt = require('bcrypt-nodejs');
 
 var User = require('../models/user');
 
+var jwt = require('../services/jwt');
+
 
 function home(req, res){
     res.status(200).send({
@@ -74,8 +76,18 @@ function loginUser(req, res){
         if(user) {
             bcrypt.compare(password, user.password, (err, check) =>{
                 if(check){
-                    //Devuelvo los datos del usuario
-                    return res.status(200).send({user});
+                    if(params.gettoken){
+                        //generar y devolver el token
+                        return res.status(200).send({
+                            token: jwt.createToken(user)
+                        });
+                    }else{
+                        //Devuelvo los datos del usuario
+                        user.password = undefined;
+                        return res.status(200).send({user});
+                    }
+
+
                 }else{
                     return res.status(404).send({message:'El usuario no se ha podido identificar'});
                 }
