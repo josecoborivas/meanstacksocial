0c2c2a03655bd326c7133d458dafcc8697212559
Jose Cobo <josecoborivas@gmail.com>
2018-03-19 12:08:52 -0300

    Middleware de autenticacion

--- api/routes/user.js
+++ api/routes/user.js
@@ -4,11 +4,12 @@ var express = require('express');
 var UserController = require('../controllers/User');
 
 var api = express.Router();
+var md_auth = require('../middlewares/authenticated');
 
 api.get('/home', UserController.home);
-api.get('/pruebas', UserController.pruebas);
+api.get('/pruebas', md_auth.ensureAuth, UserController.pruebas);
 api.post('/register', UserController.saveUser);
-api.post('/login', UserController.loginUser);
+api.post('/login',md_auth.ensureAuth, UserController.loginUser);
 
 
 module.exports = api;
