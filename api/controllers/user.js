41d993386acb73afc87a62d0118544d2e1aebded
josecoborivas <josecoborivas@gmail.com>
2018-03-30 11:28:54 -0300

    Metodo getImageFile para devolver la imagen del usuario

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -216,7 +216,18 @@ function uploadImage(req, res) {
 
 }
 
+function getImageFile(req, res){
+  var image_file = req.params.imageFile;
+  var path_file = './uploads/users/'+image_file;
 
+  fs.exists(path_file, (exists) => {
+      if(exists){
+          res.sendFile(path.resolve(path_file));
+      }else{
+          res.status(200).send({message: 'No existe la imagen'});
+      }
+  });
+}
 
 module.exports = {
     home,
@@ -226,5 +237,6 @@ module.exports = {
     getUser,
     getUsers,
     updateUser,
-    uploadImage
+    uploadImage,
+    getImageFile
 }
