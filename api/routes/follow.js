a4a4f63fdf91537cdd988e5371dd1c1d23ea03cb
josecoborivas <josecoborivas@gmail.com>
2018-04-03 08:44:25 -0300

    Metodo deleteFollow para dejar de seguir a un usuario

--- api/routes/follow.js
+++ api/routes/follow.js
@@ -6,5 +6,6 @@ var api = express.Router();
 var md_auth = require('../middlewares/authenticated');
 
 api.post('/follow', md_auth.ensureAuth, FollowControllers.saveFollow);
+api.delete('/follow/:id', md_auth.ensureAuth, FollowControllers.deleteFollow);
 
 module.exports = api;
