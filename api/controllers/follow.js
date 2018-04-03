6a0c2091eee8b4790c95de9f49d075f7eca0486f
josecoborivas <josecoborivas@gmail.com>
2018-04-03 09:57:44 -0300

    Metodo getFollowingUser para listar los usuarios que sigue un usuario determinado

--- api/controllers/follow.js
+++ api/controllers/follow.js
@@ -1,6 +1,6 @@
 'use strict'
 
-//var path = require('path');
+var path = require('path');
 //var fs = require('fs');
 var mongoosePaginate = require('mongoose-pagination');
 
@@ -34,7 +34,36 @@ function deleteFollow(req, res){
     });
 }
 
+function getFollowingUsers(req, res){
+    var userId = req.user.sub;
+
+    if(req.params.id){
+        userId = req.params.id;
+    }
+
+    var page = 1;
+
+    if(req.params.page){
+        page = req.params.page;
+    }
+
+    var itemsPerPage = 4;
+
+    Follow.find({user: userId}).populate({path: 'followed'}).paginate(page, itemsPerPage, (err, follows, total) => {
+        if(err) return res.status(500).send({message: 'Error en el servidor al paginar los usuarios que sigues'});
+
+        if(!follows) return res.status(404).send({message: 'No estas siguiendo a ningun usuario'});
+
+        return res.status(200).send({
+            total: total,
+            pages: Math.ceil(total/itemsPerPage),
+            follows
+        });
+    });
+}
+
 module.exports = {
     saveFollow,
-    deleteFollow
+    deleteFollow,
+    getFollowingUsers
 }
--- api/routes/follow.js
+++ api/routes/follow.js
@@ -7,5 +7,6 @@ var md_auth = require('../middlewares/authenticated');
 
 api.post('/follow', md_auth.ensureAuth, FollowControllers.saveFollow);
 api.delete('/follow/:id', md_auth.ensureAuth, FollowControllers.deleteFollow);
+api.get('/following/:id?/:page?', md_auth.ensureAuth, FollowControllers.getFollowingUsers);
 
 module.exports = api;
