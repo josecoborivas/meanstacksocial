1230590f31d0e060a1b8616ecb618d69c2c8a914
josecoborivas <josecoborivas@gmail.com>
2018-04-05 09:23:03 -0300

    Metodo getMyFollow para listar usuarios

--- api/controllers/follow.js
+++ api/controllers/follow.js
@@ -37,7 +37,7 @@ function deleteFollow(req, res){
 function getFollowingUsers(req, res){
     var userId = req.user.sub;
 
-    if(req.params.id){
+    if(req.params.id && req.params.page){
         userId = req.params.id;
     }
 
@@ -62,8 +62,56 @@ function getFollowingUsers(req, res){
     });
 }
 
+function getFollowedUsers(req, res){
+    var userId = req.user.sub;
+
+    if(req.params.id && req.params.page){
+        userId = req.params.id;
+    }
+
+    var page = 1;
+
+    if(req.params.page){
+        page = req.params.page;
+    }
+
+    var itemsPerPage = 4;
+
+    Follow.find({followed: userId}).populate('user').paginate(page, itemsPerPage, (err, follows, total) => {
+        if(err) return res.status(500).send({message: 'Error en el servidor al paginar los usuarios que sigues'});
+
+        if(!follows) return res.status(404).send({message: 'No te sigue ningun usuario'});
+
+        return res.status(200).send({
+            total: total,
+            pages: Math.ceil(total/itemsPerPage),
+            follows
+        });
+    });
+}
+
+//Devolver listado de usuarios
+function getMyFollows(req, res){
+    var userId = req.user.sub;
+    var find = Follow.find({user: userId});
+    
+    if(req.params.followed){
+        find = Follow.find({followed: userId});
+    }
+
+    find.populate('user followed').exec((err, follows) =>{
+        if(err) return res.status(500).send({message: 'Error en el servidor al paginar los usuarios que sigues'});
+
+        if(!follows) return res.status(404).send({message: 'No te sigue ningun usuario'});
+
+        return res.status(200).send({follows});
+    });
+}
+
 module.exports = {
     saveFollow,
     deleteFollow,
-    getFollowingUsers
+    getFollowingUsers,
+    getFollowedUsers,
+    getMyFollows
 }
