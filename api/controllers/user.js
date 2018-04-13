a7710b144b744bbb270a95b1f5026b91dbd832ff
josecoborivas <josecoborivas@gmail.com>
2018-04-13 09:35:19 -0300

    Modificacion en getUser. Se crea la funcion followThisUser que es async

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -117,13 +117,40 @@ function getUser(req, res) {
 
         if (!user) return res.status(404).send({ message: 'El usuario no existe' });
 
-        Follow.findOne({"user": req.user.sub, "followed": userId}).exec((err, follow)=> {
-            if (err) return res.status(500).send({ message: 'Error al comprobar el seguimiento' });
-
-            return res.status(200).send({ user, follow });
+        followThisUser(req.user.sub, userId).then((value) =>{
+            return res.status(200).send({ 
+                user,
+                following: value.following,
+                followed: value.followed
+             });
         });
-
+     
     });
+}
+
+async function followThisUser(identity_user_id, user_id){
+    try {
+        var following = await Follow.findOne({ user: identity_user_id, followed: user_id})
+            .then((following) => {
+                return following;
+            })
+            .catch((err)=>{
+                return handleError(err);
+            });
+        var followed = await Follow.findOne({ user: user_id, followed: identity_user_id})
+            .then((followed) => {
+                return followed;
+            })
+            .catch((err)=>{
+                return handleError(err);
+            });
+        return {
+            following: following,
+            followed: followed
+        }
+    } catch(err){
+        return handleError(err);
+    }
 }
 
 //Devolver listado de usuarios paginado
